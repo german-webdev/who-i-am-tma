bc5af27365636315d615f41d438db18bcf8b594a
musique128 <frontendue@gmail.com>
2025-03-28 00:22:10 +1000

    change homepage

--- src/server/bot.js
+++ src/server/bot.js
@@ -1,10 +1,9 @@
-const { Bot } = require("grammy");
+import { Bot } from "grammy";
 
 // Create a bot object
 const bot = new Bot("7681418703:AAGvU7hBpr9eJBYBrMnZRpiyO9gho90lcuY"); // <-- place your bot token in this string
 
-// Register listeners to handle messages
-bot.on("message:text", (ctx) => ctx.reply("Echo: " + ctx.message.text));
+// Reply to any message with "Hi there!".
+bot.on("message", (ctx) => ctx.reply("Hi there!"));
 
-// Start the bot (using long polling)
 bot.start();
